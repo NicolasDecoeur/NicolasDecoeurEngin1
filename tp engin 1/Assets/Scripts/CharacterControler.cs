47ee53a636a413f670440ac6c901f36865c054b3
NicolasDecoeur <nicolas_decoeur@hotmail.com>
2023-09-03 11:31:17 -0400

    add todo

--- tp engin 1/Assets/Scripts/CharacterControler.cs
+++ tp engin 1/Assets/Scripts/CharacterControler.cs
@@ -8,14 +8,12 @@ public class CharacterControler : MonoBehaviour
     [SerializeField] private float m_accelerationValue;
     [SerializeField] private float m_maxVelocity;  
 
-    // Start is called before the first frame update
     void Start()
     {
         m_Camera = Camera.main;
         m_rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         var vectorOnFloor = Vector3.ProjectOnPlane(m_Camera.transform.forward, Vector3.up);
@@ -31,5 +29,12 @@ public class CharacterControler : MonoBehaviour
             m_rb.velocity = m_rb.velocity.normalized;
             m_rb.velocity *= m_maxVelocity;
         }
+
+        //TODO 31 AOÛT:
+            //Appliquer les déplacements relatifs à la caméra dans les 3 autres directions
+            //Avoir des vitesses de déplacements maximales différentes vers les côtés et vers l'arrière
+            //Lorsqu'aucun input est mis, décélérer le personnage rapidement
+
+        Debug.Log(m_rb.velocity.magnitude);
     }
 }
